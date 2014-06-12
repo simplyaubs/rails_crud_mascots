a2e2712adf605fb47aa86cba06014645e73877da
Aubrey Howell <simplyaubs@gmail.com>
2014-06-12 13:06:52 -0600

    User can update a mascot from list.

--- app/controllers/mascots_controller.rb
+++ app/controllers/mascots_controller.rb
@@ -15,6 +15,21 @@ class MascotsController < ApplicationController
     end
   end
 
+  def show
+    @mascot = Mascot.find(params[:id])
+  end
+
+  def edit
+    @mascot = Mascot.find(params[:id])
+  end
+
+  def update
+    @mascot = Mascot.find(params[:id])
+    @mascot.update_attributes!(mascot_params)
+
+    redirect_to mascots_path
+  end
+
   private
   def mascot_params
     params.require(:mascot).permit(:company, :company_mascot)
--- spec/features/mascots_spec.rb
+++ spec/features/mascots_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD company mascots' do
     expect(page).to have_content 'Frontier'
     expect(page).to have_content 'Fox'
   end
+
+  scenario 'User can update a mascot' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a mascot'
+    fill_in 'Company', with: 'Frontier'
+    fill_in 'Company mascot', with: 'Fox'
+    click_on 'Add mascot'
+    expect(page).to have_content 'Frontier'
+    expect(page).to have_content 'Fox'
+    click_on 'Frontier'
+    expect(page).to have_content 'Frontier'
+    expect(page).to have_content 'Fox'
+    click_on 'Edit'
+    fill_in 'Company', with: 'Delta'
+    fill_in 'Company mascot', with: 'Triangle'
+    click_on 'Update mascot'
+    expect(page).to have_content 'Delta'
+    expect(page).to have_content 'Triangle'
+    expect(page).to_not have_content 'Frontier'
+    expect(page).to_not have_content 'Fox'
+  end
 end
